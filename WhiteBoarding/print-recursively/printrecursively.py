c470ec84857c1c70b9a092555297f36d2bf66a62
Becca Saines <rebecca.saines@gmail.com>
2016-12-14 11:59:22 -0800

    Recursive Index Complete

---
+++ WhiteBoarding/print-recursively/printrecursively.py
@@ -0,0 +1,27 @@
+"""Print items in the list, using recursion.
+
+For example::
+
+   >>> print_recursively([1, 2, 3])
+   1
+   2
+   3
+
+"""
+
+
+def print_recursively(lst):
+    """Print items in the list, using recursion."""
+
+    while lst:
+        print lst[0]
+        return print_recursively(lst[1:])
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TEST PASSED. GREAT JOB!\n"
+
+# SOLUTION:
+# used "if" instead of while. 
---
+++ WhiteBoarding/recursive-index/recursiveindex.py
@@ -0,0 +1,47 @@
+"""Given list (haystack), return index (0-based) of needle in the list.
+
+    Return None if needle is not in haystack.
+
+    Do this with recursion. You MAY NOT USE A `for` OR `while` LOOP.
+
+For example:
+
+    >>> recursive_index(5, [1, 3, 5, 2, 4])
+    2
+
+    >>> recursive_index("hey", ["hey", "there", "you"])
+    0
+
+    >>> recursive_index("you", ["hey", "there", "you"])
+    2
+
+    >>> recursive_index("zork", ["foo", "bar", "baz"]) is None
+    True
+
+"""
+
+
+def recursive_index(needle, haystack):
+    """Given list (haystack), return index (0-based) of needle in the list.
+
+    Return None if needle is not in haystack.
+
+    Do this with recursion. You MAY NOT USE A `for` OR `while` LOOP.
+    """
+    def _recursive_index(needle, haystack, i):
+
+        if i >= len(haystack):
+            return None
+
+        if haystack[i] == needle:
+            return i
+        
+        return _recursive_index(needle, haystack, i + 1)
+
+    return _recursive_index(needle, haystack, 0)
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. GO GO GO!\n"
