bf92345b511e9a3e338c857b8eb53856c5708d06
Becca Saines <rebecca.saines@gmail.com>
2016-12-15 12:03:22 -0800

    Linked Lists Practice

---
+++ WhiteBoarding/rev-linked-list/reversell.py
@@ -0,0 +1,62 @@
+"""Given linked list head node, return head node of new, reversed linked list.
+
+For example:
+
+    >>> ll = Node(1, Node(2, Node(3)))
+    >>> reverse_linked_list(ll).as_string()
+    '321'
+"""
+
+
+class Node(object):
+    """Class in a linked list."""
+
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def as_string(self):
+        """Represent data for this node and it's successors as a string.
+
+        >>> Node(3).as_string()
+        '3'
+
+        >>> Node(3, Node(2, Node(1))).as_string()
+        '321'
+        """
+
+        out = []
+        n = self
+
+        while n:
+            out.append(str(n.data))
+            n = n.next
+
+        return "".join(out)
+
+
+def reverse_linked_list(head):
+    """Given LL head node, return head node of new, reversed linked list.
+
+    >>> ll = Node(1, Node(2, Node(3)))
+    >>> reverse_linked_list(ll).as_string()
+    '321'
+    """
+
+    out_head = None
+    n = head
+
+    while n:
+        out_head = Node(n.data, out_head)
+        n = n.next
+
+    return out_head
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. RIGHT ON!\n"
+
+
+# I am atrocious at linked lists.  I needed full assistance on this one.
