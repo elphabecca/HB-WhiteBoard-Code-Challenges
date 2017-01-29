91d10ce5ac46fbfb52997075d34ed72d79249b98
Becca Saines <rebecca.saines@gmail.com>
2017-01-29 10:32:24 -0800

    shift array updated

--- shiftarray.py
+++ shiftarray.py
@@ -1,5 +1,5 @@
 
-def shift_array(num_list, offset):
+def shift_array2(num_list, offset):
 
     offset = offset % len(num_list)
 
@@ -16,10 +16,21 @@ def shift_array(num_list, offset):
             curr_value = replace_value
 
     return num_list
-            
 
-    # replace_index = (curr_index - 2) % len(num_list)
-    # replace_value = num_list[replace_index]
-    # num_list[replace_index] = curr_value
-    # curr_value = replace_value
-    # curr_index = replace index
+def shift_array1(num_list, offset):
+
+    offset = offset % len(num_list)
+
+    if offset == 0:
+        return num_list
+
+    curr_value = num_list[0]
+    curr_index = 0
+    for i in range(len(num_list)):
+        replace_index = (curr_index - offset) % len(num_list)
+        replace_value = num_list[replace_index]
+        num_list[replace_index] = curr_value
+        curr_value = replace_value
+        curr_index = replace index
+
+    return num_list 
