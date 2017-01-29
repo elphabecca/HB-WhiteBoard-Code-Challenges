a0e52d31bbf4f69541451ad93817f0aa9f564e48
Becca Saines <rebecca.saines@gmail.com>
2017-01-29 10:44:21 -0800

    shiftarray clean up

--- shiftarray.py
+++ shiftarray.py
@@ -1,22 +1,4 @@
 
-def shift_array2(num_list, offset):
-
-    offset = offset % len(num_list)
-
-    if offset == 0:
-        return num_list
-
-    curr_value = num_list[0]
-    for j in range(offset):
-        print j, num_list           
-        for i in range(len(num_list)):
-            replace_index = i - 1
-            replace_value = num_list[replace_index]
-            num_list[replace_index] = curr_value
-            curr_value = replace_value
-
-    return num_list
-
 def shift_array1(num_list, offset):
 
     offset = offset % len(num_list)
@@ -31,6 +13,26 @@ def shift_array1(num_list, offset):
         replace_value = num_list[replace_index]
         num_list[replace_index] = curr_value
         curr_value = replace_value
-        curr_index = replace index
+        curr_index = replace_index
 
     return num_list 
+
+
+def shift_array2(num_list, offset):
+
+    offset = offset % len(num_list)
+
+    if offset == 0:
+        return num_list
+
+    for j in range(offset):
+        curr_value = num_list[0]
+        k = -1
+        for i in range(len(num_list)):
+            replace_index = k
+            replace_value = num_list[replace_index]
+            num_list[replace_index] = curr_value
+            curr_value = replace_value
+            k -= 1
+
+    return num_list
